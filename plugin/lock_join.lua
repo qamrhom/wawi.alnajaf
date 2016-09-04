7c0b9742001aa656d4298c343bae8df3843a69fa
qamrhom <colonel8757@gmail.com>
2016-09-04 20:35:49 +0300

    Add files via upload

---
+++ plugin/lock_join.lua
@@ -0,0 +1,32 @@
+--[[
+#
+#ـــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+#:((#:((
+# For More Information ....! 
+# wawi : alnajaf < @wawi_alnajaf > 
+# our channel: @almlaek
+# Version: 1.1
+#:))
+#ــ
+#ــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــــ
+#
+]]
+local function run (msg, matches)
+local data = load_data(_config.moderation.data)
+   if matches[1] == 'chat_add_user_link' then
+        local user_id = msg.from.id
+        if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+        if data[tostring(msg.to.id)]['settings']['join'] == 'yes' then
+      kick_user(user_id, msg.to.id)
+    end
+   end
+end   
+ end
+ end
+return {
+  patterns = {
+    "^!!tgservice (chat_add_user_link)$"
+  },
+  run = run
+}
